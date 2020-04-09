818d0c19a6ee0cb8529bf8da173c3398b0a8a260
lambrosopos <lambrosopos@gmail.com>
2020-04-09 19:01:27 +0900

    :alien: Updating endpoints
     - Switching user to users

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/user', userRouter);
+app.use('/users', userRouter);
 app.use('/articles', articlesRouter)
 app.use('/scores', scoreRouter)
 
