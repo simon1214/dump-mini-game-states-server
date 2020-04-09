c51c68c44912ad55d89f689fb49fd92878062acf
lambrosopos <lambrosopos@gmail.com>
2020-04-09 16:20:05 +0900

    :construction: Implementing user endpoints
     - Updated signin, signout, signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,43 @@
+const {Users} = require('../models')
+
 const userController = {
   signin : (req, res) => {
-    res.sendStatus(200)
+    const {username, password} = req.body
+    Users.findOne({
+      where:username
+    }).then(result => {
+      if (!result) {
+        res.sendStatus(404)
+      }
+
+      if (result.dataValues.password !== password) {
+        res.sendStatus(401)
+      } else {
+        res.status(200).json({token:"temptoken"})
+      }
+    })
+    
+    
   },
   signup : (req, res) => {
-    res.sendStatus(200)
+    const {username, password, nickname, email} = req.body
+
+    Users.findOrCreate({
+      where: {
+        username
+      },
+      defaults: {
+        password,
+        nickname,
+        email
+      }
+    }).then(([result, created]) => {
+      if (!created) {
+        res.sendStatus(409)
+      } else {
+        res.sendStatus(201)
+      }
+    })
   },
   signout : (req, res) => {
     res.sendStatus(200)
