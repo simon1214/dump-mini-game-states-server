0d1316d3f86842be7e09ac99b92a4327dddbd269
lambrosopos <lambrosopos@gmail.com>
2020-04-09 19:46:43 +0900

    :building_construction: Changing middleware structure
     - JWT Authenticator now operating on designed access token secret

--- middlewares/jwtAuthenticator.js
+++ middlewares/jwtAuthenticator.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
-const accessTokenSecret = process.env.accessTokenSecret
+const accessTokenSecret = process.env.accessTokenSecret || "@development"
 
 const jwtAuthenticator = (req, res, next) => {
   const authHeader = req.headers.authorization
-
   if (authHeader) {
     jwt.verify(authHeader, accessTokenSecret, (err, user) => {
       if (err) res.sendStatus(403)
