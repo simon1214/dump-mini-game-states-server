7be982cf0eb5e0874f0c69ac19891423c31d12a6
lambrosopos <lambrosopos@gmail.com>
2020-04-09 18:57:48 +0900

    :alien: Updating routes
     - Updated articles routes

--- routes/articles.js
+++ routes/articles.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const articlesController = require('../controllers').articlesController
 
+const jwtAuthenticator = require('../middlewares/jwtAuthenticator')
+
 router.get('/', articlesController.getArticles)
+router.post('/', jwtAuthenticator, articlesController.postArticle)
 
 module.exports = router
