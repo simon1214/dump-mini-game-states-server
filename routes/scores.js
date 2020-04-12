ce04168e6a69592fe7bae3ae5dd03ccba380f762
lambrosopos <lambrosopos@gmail.com>
2020-04-12 11:53:51 +0900

    :lock: Adding JWT Authenticator to routes

--- routes/scores.js
+++ routes/scores.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { scoreController } = require('../controllers');
+const { jwtAuthenticator } = require('../middlewares/jwtAuthenticator');
+
 const router = express.Router();
 
-router.get('/scores', scoreController.getUserScores);
-router.post('/scores', scoreController.postUserScores);
+router.get('/scores', jwtAuthenticator, scoreController.getUserScores);
+router.post('/scores', jwtAuthenticator, scoreController.postUserScores);
 router.get('/leaderboard', scoreController.getLeaderboard);
 
 module.exports = router;
