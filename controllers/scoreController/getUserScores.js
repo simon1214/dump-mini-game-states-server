e8849f4ab2c7599614535fcedeb2600869da4f38
lambrosopos <lambrosopos@gmail.com>
2020-04-12 12:29:03 +0900

    :sparkles: Added get score method
     - Returns an array of scores according to the user in JWT token

--- controllers/scoreController/getUserScores.js
+++ controllers/scoreController/getUserScores.js
@@ -1,5 +1,20 @@
+const { Scores } = require('../../models');
+
 const getUserScores = (req, res) => {
-  res.sendStatus(200);
+  const user_id = req.user.sub;
+  Scores.findAll({ where: { user_id } })
+    .then((rawResult) => {
+      return rawResult.map((rawData) => {
+        const { user_id, game_id, score, created_at } = rawData.dataValues;
+        return { user_id, game_id, score, created_at };
+      });
+    })
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch(() => {
+      res.status(400).send("Couldn't search for the scores");
+    });
 };
 
 module.exports = getUserScores;
