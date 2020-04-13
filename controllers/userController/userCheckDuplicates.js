1069a2d3c34c2a486a1e536e8b57ffdc45ba8e28
lambrosopos <lambrosopos@gmail.com>
2020-04-13 11:10:10 +0900

    :ambulance: Fixing checkID, checkNickname

--- controllers/userController/userCheckDuplicates.js
+++ controllers/userController/userCheckDuplicates.js
@@ -3,7 +3,7 @@ const { Users } = require('../../models');
 const userCheckDuplicates = {
   checkDuplicateID: (req, res) => {
     const { username } = req.body;
-    Users.find({ where: username }).then((result) => {
+    Users.findOne({ where: { username } }).then((result) => {
       if (result) {
         res.status(200).send('Username already exists');
       } else {
@@ -13,7 +13,7 @@ const userCheckDuplicates = {
   },
   checkDuplicateNickname: (req, res) => {
     const { nickname } = req.body;
-    Users.find({ where: nickname }).then((result) => {
+    Users.findOne({ where: { nickname } }).then((result) => {
       if (result) {
         res.status(200).send('Nickname taken!');
       } else {
